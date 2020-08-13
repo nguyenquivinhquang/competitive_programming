36498059891f74a879d3aba19c568b4786d11a50
Chichcpu <nguyenquivinhquang@gmail.com>
2020-08-13 16:23:23 +0700

    quang

---
+++ cses/1139.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define N 100
+#define ll long long
+#define pp pair<int, int>
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define reset(x) memset(x, 0, sizeof(x))
+#define quyen_sort(x, size) sort(x + 1, x + 1 + size);
+using namespace std;
+int main()
+{
+    int testcase;
+}
